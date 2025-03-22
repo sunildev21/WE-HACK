52459bc793ee4bea15bdb26026aa523081a9a14f
gaurav <prajapatigaurav1301@gmail.com>
2025-03-22 17:27:57 +0530

    second repo

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env: {
+    WORKOS_API_KEY: process.env.WORKOS_API_KEY,
+    NEXT_PUBLIC_WORKOS_CLIENT_ID: process.env.NEXT_PUBLIC_WORKOS_CLIENT_ID,
+    NEXT_PUBLIC_WORKOS_REDIRECT_URI: process.env.NEXT_PUBLIC_WORKOS_REDIRECT_URI,
+  }
 };
 
 export default nextConfig;
